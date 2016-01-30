e31a8e0695198a204d30eed4d1a5451a906f1775
James Page <james.page@ubuntu.com>
2016-01-30 16:58:45 +0100

    Update setup

--- setup.sh
+++ setup.sh
@@ -2,8 +2,9 @@
 
 set -ex
 
-echo "Pre-loading kernel modules"
-modprobe openvswitch
-modprobe nbd
-modprobe ip6_tables
-modprobe ip_tables
+mkdir -p trusty
+
+[ -d trusty/ceph ] || bzr co --lightweight lp:~james-page/charms/trusty/ceph/lp1535315 trusty/ceph
+[ -d trusty/ceph-osd ] || bzr co --lightweight lp:~james-page/charms/trusty/ceph-osd/lp1535315 trusty/ceph-osd
+
+cat lxd-profile.yaml | lxc profile edit juju-openstack-on-lxd
