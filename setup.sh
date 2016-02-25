07618cdeb8245cb1de6f3e29005e1badc406ad5a
James Page <james.page@ubuntu.com>
2016-02-25 11:37:49 +0000

    Use cs branches as changes have landed

--- setup.sh
+++ setup.sh
@@ -2,9 +2,4 @@
 
 set -ex
 
-mkdir -p trusty
-
-[ -d trusty/ceph ] || bzr co --lightweight lp:~james-page/charms/trusty/ceph/lp1535315 trusty/ceph
-[ -d trusty/ceph-osd ] || bzr co --lightweight lp:~james-page/charms/trusty/ceph-osd/lp1535315 trusty/ceph-osd
-
 cat lxd-profile.yaml | lxc profile edit juju-openstack-on-lxd
