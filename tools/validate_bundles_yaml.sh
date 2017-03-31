3f51782718c5c9d3126e97ad719b63fe54139f63
Ryan Beisner <ryan.beisner@canonical.com>
2017-03-31 14:39:06 -0500

    Update test

---
+++ tools/validate_bundles_yaml.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -ex
+
+# Expect the only difference across these bundles to be source/openstack-origin
+# This check is not complete or perfect, but will catch many variances.
+base_comparison_bundle=bundle-mitaka.yaml
+allowable_diff="source|options|origin|yaml"
+for b in bundle-ocata.yaml bundle-newton.yaml; do
+  if diff -Naur $b $base_comparison_bundle | egrep '^(\+|\-)' | egrep -v "$allowable_diff"; then
+    echo "FAIL: $b:$base_comparison_bundle comparison NOT ok (too much diff)"
+    exit 1
+  fi
+done
+
+# Synthetically validate bundle for yaml and Juju syntax
+bundles=$(find . -name "bundle*.yaml")
+for bundle in $bundles; do
+    /usr/bin/env python -c 'import yaml,sys;yaml.safe_load(sys.stdin)' < $bundle
+done
