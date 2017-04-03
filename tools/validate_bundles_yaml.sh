28b6c81c9f688ea7c7092bd437697e6ca308663a
Ryan Beisner <ryan.beisner@canonical.com>
2017-04-03 09:50:19 -0500

    Also check diffs of the s390x bundle files in tox

--- tools/validate_bundles_yaml.sh
+++ tools/validate_bundles_yaml.sh
@@ -11,6 +11,15 @@ for b in bundle-ocata.yaml bundle-newton.yaml; do
   fi
 done
 
+## s390x bundle file comparisons
+base_comparison_bundle=bundle-mitaka-s390x.yaml
+for b in bundle-ocata-s390x.yaml bundle-newton-s390x.yaml; do
+  if diff -Naur $b $base_comparison_bundle | egrep '^(\+|\-)' | egrep -v "$allowable_diff"; then
+    echo "FAIL: $b:$base_comparison_bundle comparison NOT ok (too much diff)"
+    exit 1
+  fi
+done
+
 # Basic yaml syntax check
 bundles=$(find . -name "bundle*.yaml")
 for bundle in $bundles; do
